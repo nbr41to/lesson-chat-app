750fc1688d908905012f2065742b2be81966e268
Knob <59198478+nbr41to@users.noreply.github.com>
2023-05-26 11:49:59 +0900

    Update Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react';
 import { Header } from '@/components/Header';
 import { FooterMenu } from '@/components/FooterMenu';
 import styles from './Layout.module.css';
+import { useRouter } from 'next/router';
 
 type Props = {
   children: ReactNode;
@@ -9,9 +10,11 @@ type Props = {
 };
 
 export const Layout: FC<Props> = ({ children, isLogin }) => {
+  const router = useRouter();
+
   return (
     <div className={styles.root}>
-      <Header />
+      {router.pathname !== '/rooms/[roomId]' && <Header />}
       <main className={styles.main}>{children}</main>
       {isLogin && <FooterMenu />}
     </div>
